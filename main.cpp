6f5cd445e0a0190e19103179bf15710b1913d03d
tylr <tylr@MBP.local>
2022-01-19 20:29:36 -0800

    Adding main file.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Tyler Taormina & Freddy Hernandez Project 1 Winter Term CST126
+//Jan 2022
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Code me." << endl;
+    return 0;
+}
