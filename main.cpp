90f24b7fbe378cb73d6183fb7e96ca8b3287ed3d
tylr <tylr@MBP.local>
2022-01-20 13:50:12 -0800

    Getting started.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,69 @@
-//Tyler Taormina & Freddy Hernandez Project 1 Winter Term CST126
-//Jan 2022
-
 #include <iostream>
+#include <string>
 
-using namespace std;
+using std::string;
+
+
+class Stock
+
+{
+public:
+    string name;
+    string symbol;
+    float price;
+
+    void get_info();
+
+    void info()
+    {
+        std::cout << "Stock name: " << name << std::endl;
+        std::cout << "Stock symbol: " << symbol << std::endl;
+        std::cout << "Stock price: " << price << std::endl;
+        std::cout << "======================================" << std::endl;
+    }
+};
+
+
+void Stock::get_info()
+{
+    std::cout << "Enter the name of the stock: ";
+    std::cin >> name;
+    std::cout << "Enter the symbol of the stock: ";
+    std::cin >> symbol;
+    std::cout << "Enter the price of the stock: ";
+    std::cin >> price;
+}
+
+
+void build_list(Stock* arr, int n)
+{
+    int i;
+    Stock sto;
+
+    for (i = 0; i < n; i++)
+    {
+        sto.get_info();
+        arr[i] = sto;
+    }
+}
+
 
 int main()
 {
-    cout << "Code me." << endl;
+    int n, i, j;
+
+    std::cout << "Enter the number of stocks you would like to initially enter: ";
+    std::cin >> n;
+    Stock arr[n];
+
+    build_list(arr, n);
+
+    for (i = 0; i < n; i++)
+    {
+        std::cout << "**STOCK " << i + 1 << "**" << std::endl;
+        arr[i].info();
+    }
+
     return 0;
+
 }
